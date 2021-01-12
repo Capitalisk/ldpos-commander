19c7c4a4454964ffb38ad772c5e226a40d584e95
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-01-12 18:37:15 +0100

    Add test suite boilerplate

---
+++ test/integration.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+describe('LDPoS Commander integration tests', async () => {
+
+  beforeEach(async () => {
+
+  });
+
+  afterEach(async () => {
+
+  });
+
+  describe('some feature', async () => {
+
+    it('should ...', async () => {
+
+    });
+
+  });
+
+});
