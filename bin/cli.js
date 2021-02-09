3810b54da926523a713410ea77eb1275c8c71654
Maarten Coppens <martinuske@gmail.com>
2021-02-09 14:18:38 -0300

    working again

--- bin/cli.js
+++ bin/cli.js
@@ -12,6 +12,7 @@ const {
   configPath,
   errorLog,
   passphrasePrompt,
+  networkSymbolPrompt,
 } = require('../lib/index');
 
 const configFile = 'ldpos-config.json';
@@ -28,11 +29,11 @@ const getConfig = async () => {
   } else {
     // If not exists prompt questions
     // prettier-ignore
-    hostname = await promptInput('Server IP: (Default: 34.227.22.98)') || '34.227.22.99';
+    hostname = await promptInput('Server IP: (Default: 34.227.22.98)') || '34.227.22.98';
     // prettier-ignore
     port = await promptInput('Port: (Default: 7001)') || 7001;
     // prettier-ignore
-    networkSymbol = await promptInput('Network symbol: (Default: ldpos)') || 'ldpos';
+    networkSymbol = await networkSymbolPrompt()
     // prettier-ignore
     save = ['Y', 'y'].includes(await promptInput(`Save in your home dir? (Y/n)`));
 
@@ -72,7 +73,7 @@ const log = () => {
   // Switch case for commands
   const sw = {
     remove: async () => await fs.remove(fullConfigPath),
-    balance: async () => await accountBalance(client),
+    balance: async (client) => await accountBalance(client),
     '--help': async () => log(),
     '-v': async () => console.log(require('./package.json').version),
     default: async () => log(),
@@ -80,36 +81,40 @@ const log = () => {
 
   try {
     if (command === 'remove') {
-      await sw.remove()
-      return
+      await sw.remove();
+      return;
     }
 
-    // Get config if existent in home dir or create config object
-    const config = await getConfig();
-
-    // Get passphrase of the wallet
-    const passphrase = await passphrasePrompt();
-
-    let client;
+    let config;
 
     // If command is an ip execute it on another server
     if (command.split('.').length === 4) {
       const hostname = command.split(':')[0];
-      const port = command.split(':')[0] || 7001;
+      const port = command.split(':')[1] || 7001;
+      const networkSymbol = await networkSymbolPrompt();
       command = argv._[1];
 
-      client = ldposClient.createClient({ ...config, hostname, port });
+      config = { networkSymbol, hostname, port };
     } else {
-      client = ldposClient.createClient(config);
+      // Get config if existent in home dir or create config object
+      config = await getConfig();
     }
+
+    // Get passphrase of the wallet
+    const passphrase = await passphrasePrompt();
+
+    console.log(config, passphrase)
+
+    const client = ldposClient.createClient(config);
+
     await client.connect({
       passphrase,
     });
 
-
-    await (sw[command] || sw.default)();
+    await (sw[command] || sw.default)(client);
     process.exit();
   } catch (e) {
+    debugger;
     errorLog(e.message);
   }
 })();
