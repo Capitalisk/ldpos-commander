61ab48621ace6c350dab68cda87d1661a3a6d87b
Maarten Coppens <martinuske@gmail.com>
2021-02-10 10:26:08 -0300

    going to implement RPEL and Non-RPEL

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,21 @@
-const argv = require('minimist')(process.argv.slice(2));
-const { CmdInterface } = require('../lib')
+#!/usr/bin/env node
 
+const argv = require('minimist')(process.argv.slice(2));
+const { CmdInterface } = require('../lib');
+
+const config = {}
+
+// i = 1 to skip _
+for (let i = 1; i < Object.keys(argv).length; i++) {
+  const arg = Object.keys(argv)[i];
+  config[arg.replace(/-./g, x=>x.toUpperCase()[1])] = argv[arg]
+}
+
+const interface = new CmdInterface(config);
+
+(async () => {
+  if (argv.hostname) {
+    if (typeof argv.hostname === 'string' && argv.hostname.split('.').length === 5)
+      interface.command(argv)
+  }
+})();
