8a4e628d51ad4e33b57d0e7b7c5146ec6e583af4
Maarten Coppens <martinuske@gmail.com>
2021-02-10 20:52:52 -0300

    kebab to camelCase working

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const argv = require('minimist')(process.argv.slice(2));
-const { CmdInterface } = require('../lib');
+const { CmdInterface, kebabCaseToCamel } = require('../lib');
 
 const config = {};
 
 // i = 1 to skip _
 for (let i = 1; i < Object.keys(argv).length; i++) {
   const arg = Object.keys(argv)[i];
-  config[arg.replace(/-./g, (x) => x.toUpperCase()[1])] = argv[arg];
+  config[kebabCaseToCamel(arg)] = argv[arg];
 }
 
 (async () => {
