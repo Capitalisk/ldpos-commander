ef2503a259adb909017f4b1fe330a28bb1c1c0fb
Maarten Coppens <martinuske@gmail.com>
2021-02-16 16:55:59 -0300

    improved action functions, no more self needed this and client is dynamically bound to the functions removed beforeCommandFn initiation now is executed inside an async function

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
+const fs = require('fs-extra');
+const ldposClient = require('ldpos-client');
 const { CliInterface } = require('@maartennnn/cli-builder');
 const actions = require('../lib/actions');
+const { FULL_CONFIG_PATH } = require('../lib/constants');
 
-const beforeCommandFn = async () => {
+const cli = new CliInterface({
+  interactive: true,
+  helpFooter: '',
+  helpHeader: 'This is shown in the header',
+  exceptions: ['clean'],
+  actions,
+});
+
+(async () => {
   // TODO: Implement ip:port
   // TODO: Clean to exceptions
   // TODO: If no hostname or port prompt them
@@ -11,260 +22,254 @@ const beforeCommandFn = async () => {
   // TODO: Save config file
   // TODO: Prompt pass phrase
   // TODO: LDPoS client connect
-  const clean = argv._.includes('clean');
-
-  actions = require('./actions');
+  let config = {};
+  let client;
 
   // If command is an ip change config to that IP
   if (
-    argv._[0] &&
-    argv._[0].includes('.') &&
-    argv._[0].split('.').length === 4
+    cli.argv._[0] &&
+    cli.argv._[0].includes('.') &&
+    cli.argv._[0].split('.').length === 4
   ) {
-    const hostname = argv._[0].split(':')[0];
-    const port = argv._[0].split(':')[1] || 7001;
+    const hostname = cli.argv._[0].split(':')[0];
+    const port = cli.argv._[0].split(':')[1] || 7001;
 
-    this.config = { hostname, port };
+    config = { hostname, port };
   }
 
-  if (!clean && !exceptionCommands) {
-    if (!this.config.hostname) {
-      if (await fs.pathExists(this.fullConfigPath)) {
-        this.config = require(this.fullConfigPath);
+  if (
+    !cli.options.exceptions
+      .map((f) => cli.argv.hasOwnProperty(f))
+      .includes(true)
+  ) {
+    if (!cli.options.hostname) {
+      if (await fs.pathExists(FULL_CONFIG_PATH)) {
+        config = require(FULL_CONFIG_PATH);
       } else {
         // prettier-ignore
-        this.config = {
-          ...this.config,
-          hostname: await this.promptInput('Server IP:') || '34.227.22.98',
-          port: await this.promptInput('Port: (Default: 7001)') || 7001,
+        config = {
+          ...config,
+          hostname: await cli.promptInput('Server IP:') || '34.227.22.98',
+          port: await cli.promptInput('Port: (Default: 7001)') || 7001,
         };
 
         const save = ['Y', 'y'].includes(
-          await this.promptInput(`Save in your home dir? (Y/n)`)
+          await cli.promptInput(`Save in your home dir? (Y/n)`)
         );
 
         if (save)
           await fs.outputFile(
-            this.fullConfigPath,
+            FULL_CONFIG_PATH,
             JSON.stringify(config, null, 2)
           );
       }
     }
 
-    if (!this.config.networkSymbol) {
-      this.config = {
-        ...this.config,
+    if (!config.networkSymbol) {
+      config = {
+        ...config,
         networkSymbol:
-          (await this.promptInput('Network symbol: (Default: ldpos)')) ||
+          (await cli.promptInput('Network symbol: (Default: ldpos)')) ||
           'ldpos',
       };
     }
 
     // Get passphrase of the wallet
-    this.passphrase =
-      (await this.promptInput('Passphrase:', true)) ||
+    config.passphrase =
+      (await cli.promptInput('Passphrase:', true)) ||
       'clerk aware give dog reopen peasant duty cheese tobacco trouble gold angle';
-    this.client = ldposClient.createClient(this.config);
+    client = ldposClient.createClient(config);
 
     try {
-      await this.client.connect({
+      await client.connect({
         passphrase: this.passphrase,
       });
+
+      cli.options.bindActionArgs = [client];
     } catch (e) {
-      this.errorLog("Can't connect to socket");
+      cli.errorLog("Can't connect to socket");
     }
   }
-};
 
-const options = {
-  interactive: true,
-  helpFooter: '',
-  helpHeader: 'This is shown in the header',
-  // beforeCommandFn,
-};
+  const ldposAction = async (fn, message, action = null) =>
+    cli.successLog(
+      typeof client[fn] === 'function' ? await client[fn](action) : client[fn],
+      message
+    );
 
-const ldposAction = async (fn, message, action = null) =>
-  this.successLog(
-    typeof this.client[fn] === 'function'
-      ? await this.client[fn](action)
-      : this.client[fn],
-    message
-  );
-
-const cli = new CliInterface();
-
-// prettier-ignore
-const commands = {
-  login: () => console.log('login'),
-  exit: () => cli.exit(),
-  v: async () => successLog(`Version: ${require('../package.json').version}`),
-  version: async () => successLog(`Version: ${require('../package.json').version}`),
-  indexes: {
-    sync: {
-      all: {
-        execute: async () => ldposAction('syncAllKeyIndexes', 'sync all update results:'),
-        help: 'test'
+  // prettier-ignore
+  const commands = {
+    login: () => console.log('login'),
+    exit: () => cli.exit(),
+    v: async () => successLog(`Version: ${require('../package.json').version}`),
+    version: async () => successLog(`Version: ${require('../package.json').version}`),
+    indexes: {
+      sync: {
+        all: {
+          execute: async () => ldposAction('syncAllKeyIndexes', 'sync all update results:'),
+          help: 'test'
+        },
+        forging: {
+          execute: async () => ldposAction('syncKeyIndex', 'sync forging update results:', 'forging'),
+          help: 'test'
+        },
+        multisig: {
+          execute: async () => ldposAction('syncKeyIndex', 'sync multisig update results:', 'multisig'),
+          help: 'test'
+        },
+        sig: {
+          execute: async () => ldposAction('syncKeyIndex', 'sync sig update results:', 'sig'),
+          help: 'test'
+        },
       },
-      forging: {
-        execute: async () => ldposAction('syncKeyIndex', 'sync forging update results:', 'forging'),
-        help: 'test'
+      verify: {
+        forging: {
+          execute: async () => ldposAction('verifyKeyIndex', 'verify forging results:', 'forging'),
+          help: 'test'
+        },
+        multisig: {
+          execute: async () => ldposAction('verifyKeyIndex', 'verify multisig results:', 'multisig'),
+          help: 'test'
+        },
+        sig: {
+          execute: async () => ldposAction('verifyKeyIndex', 'verify sig results:', 'sig'),
+          help: 'test'
+        },
       },
-      multisig: {
-        execute: async () => ldposAction('syncKeyIndex', 'sync multisig update results:', 'multisig'),
-        help: 'test'
+      load: {
+        forging: {
+          execute: async () => ldposAction('loadKeyIndex', 'verify forging results:', 'forging'),
+          help: 'test'
+        },
+        multisig: {
+          execute: async () => ldposAction('loadKeyIndex', 'verify multisig results:', 'multisig'),
+          help: 'test'
+        },
+        sig: {
+          execute: async () => ldposAction('loadKeyIndex', 'verify sig results:', 'sig'),
+          help: 'test'
+        },
       },
-      sig: {
-        execute: async () => ldposAction('syncKeyIndex', 'sync sig update results:', 'sig'),
-        help: 'test'
-      },
-    },
-    verify: {
-      forging: {
-        execute: async () => ldposAction('verifyKeyIndex', 'verify forging results:', 'forging'),
-        help: 'test'
-      },
-      multisig: {
-        execute: async () => ldposAction('verifyKeyIndex', 'verify multisig results:', 'multisig'),
-        help: 'test'
-      },
-      sig: {
-        execute: async () => ldposAction('verifyKeyIndex', 'verify sig results:', 'sig'),
-        help: 'test'
-      },
-    },
-    load: {
-      forging: {
-        execute: async () => ldposAction('loadKeyIndex', 'verify forging results:', 'forging'),
-        help: 'test'
-      },
-      multisig: {
-        execute: async () => ldposAction('loadKeyIndex', 'verify multisig results:', 'multisig'),
-        help: 'test'
-      },
-      sig: {
-        execute: async () => ldposAction('loadKeyIndex', 'verify sig results:', 'sig'),
-        help: 'test'
-      },
-    },
-    save: {
-      forging: {
-        execute: async () => ldposAction('loadKeyIndex', 'verify forging results:', 'forging'),
-        help: 'test'
-      },
-      multisig: {
-        execute: async () => ldposAction('loadKeyIndex', 'verify multisig results:', 'multisig'),
-        help: 'test'
-      },
-      sig: {
-        execute: async () => ldposAction('loadKeyIndex', 'verify sig results:', 'sig'),
-        help: 'test'
+      save: {
+        forging: {
+          execute: async () => ldposAction('loadKeyIndex', 'verify forging results:', 'forging'),
+          help: 'test'
+        },
+        multisig: {
+          execute: async () => ldposAction('loadKeyIndex', 'verify multisig results:', 'multisig'),
+          help: 'test'
+        },
+        sig: {
+          execute: async () => ldposAction('loadKeyIndex', 'verify sig results:', 'sig'),
+          help: 'test'
+        },
       },
     },
-  },
-  wallet: {
-    balance: {
-      execute: async () => await actions.getBalance(),
-      help: 'test'
+    wallet: {
+      balance: {
+        execute: async () => await cli.actions.getBalance(),
+        help: 'test'
+      },
+      address: {
+        execute: async () => ldposAction('getWalletAddress', 'wallet address:'),
+        help: 'test'
+      },
+      generate: {
+        execute: async () => ldposAction('generateWallet', 'generated wallet:'),
+        help: 'test'
+      },
+      get: {
+        execute: async () => await cli.actions.getWallet(),
+        help: 'test'
+      },
+      getMultisigWalletMembers: {
+        execute: async () => await cli.actions.getMultisigWalletMembers(),
+        help: 'test'
+      },
+      publicKey: {
+        execute: async () => ldposAction('sigPublicKey', 'public key:'),
+        help: 'test'
+      },
+      multisigPublicKey: {
+        execute: async () => ldposAction('multisigPublicKey', 'public key:'),
+        help: 'test'
+      },
     },
-    address: {
-      execute: async () => ldposAction('getWalletAddress', 'wallet address:'),
-      help: 'test'
+    config: {
+      clean: {
+        execute: async () => await fs.remove(cli.fullConfigPath),
+        help: 'test'
+      },
+      networkSymbol: {
+        execute: async () => ldposAction('getNetworkSymbol', 'sync all update results:'),
+        help: 'test'
+      },
     },
-    generate: {
-      execute: async () => ldposAction('generateWallet', 'generated wallet:'),
-      help: 'test'
+    transaction: {
+      transfer: {
+        execute: async () => await cli.actions.transfer(),
+        help: 'test'
+      },
+      vote: {
+        execute: async () => await cli.actions.vote(),
+        help: 'test'
+      },
+      unvote: {
+        execute: async () => await cli.actions.unvote(),
+        help: 'test'
+      },
+      multisignTransfer: {
+        execute: async () => await cli.actions.multisignTransfer(),
+        help: 'test'
+      },
+      verify: {
+        execute: async () => await cli.actions.verifyTransaction(),
+        help: 'test'
+      },
+      registerMultisigWallet: {
+        execute: async () => await cli.actions.registerMultisigWallet(),
+        help: 'test'
+      },
+      registerMultisigDetails: {
+        execute: async () => await cli.actions.registerMultisigDetails(),
+        help: 'test'
+      },
+      registerSigDetails: {
+        execute: async () => await cli.actions.registerSigDetails(),
+        help: 'test'
+      },
+      registerForgingDetails: {
+        execute: async () => await cli.actions.registerForgingDetails(),
+        help: 'test'
+      },
     },
-    get: {
-      execute: async () => await actions.getWallet(),
-      help: 'test'
-    },
-    getMultisigWalletMembers: {
-      execute: async () => await actions.getMultisigWalletMembers(),
-      help: 'test'
-    },
-    publicKey: {
-      execute: async () => ldposAction('sigPublicKey', 'public key:'),
-      help: 'test'
-    },
-    multisigPublicKey: {
-      execute: async () => ldposAction('multisigPublicKey', 'public key:'),
-      help: 'test'
-    },
-  },
-  config: {
-    clean: {
-      execute: async () => await fs.remove(cli.fullConfigPath),
-      help: 'test'
-    },
-    networkSymbol: {
-      execute: async () => ldposAction('getNetworkSymbol', 'sync all update results:'),
-      help: 'test'
-    },
-  },
-  transaction: {
-    transfer: {
-      execute: async () => await actions.transfer(),
-      help: 'test'
-    },
-    vote: {
-      execute: async () => await actions.vote(),
-      help: 'test'
-    },
-    unvote: {
-      execute: async () => await actions.unvote(),
-      help: 'test'
-    },
-    multisignTransfer: {
-      execute: async () => await actions.multisignTransfer(),
-      help: 'test'
-    },
-    verify: {
-      execute: async () => await actions.verifyTransaction(),
-      help: 'test'
-    },
-    registerMultisigWallet: {
-      execute: async () => await actions.registerMultisigWallet(),
-      help: 'test'
-    },
-    registerMultisigDetails: {
-      execute: async () => await actions.registerMultisigDetails(),
-      help: 'test'
-    },
-    registerSigDetails: {
-      execute: async () => await actions.registerSigDetails(),
-      help: 'test'
-    },
-    registerForgingDetails: {
-      execute: async () => await actions.registerForgingDetails(),
-      help: 'test'
-    },
-  },
-  account: {
-    balance: {
-      execute: async () => await actions.balance(),
-      help: 'test'
-    },
-    transactions: {
-      execute: async () => await actions.transactions(),
-      help: 'test'
-    },
-    votes: {
-      execute: async () => await actions.votes(),
-      help: 'test'
-    },
-    block: {
-      execute: async () => await actions.block(),
-      help: 'test'
-    },
-    list: {
-      execute: async () => await actions.list(),
-      help: 'test'
-    },
-    pendingTransactions: {
-      execute: async () => await actions.pendingTransactions(),
-      help: 'test'
-    },
+    account: {
+      balance: {
+        execute: async () => await cli.actions.balance(),
+        help: 'test'
+      },
+      transactions: {
+        execute: async () => await cli.actions.transactions(),
+        help: 'test'
+      },
+      votes: {
+        execute: async () => await cli.actions.votes(),
+        help: 'test'
+      },
+      block: {
+        execute: async () => await cli.actions.block(),
+        help: 'test'
+      },
+      list: {
+        execute: async () => await cli.actions.list(),
+        help: 'test'
+      },
+      pendingTransactions: {
+        execute: async () => await cli.actions.pendingTransactions(),
+        help: 'test'
+      },
+    }
   }
-}
 
-cli.run(commands, options);
+  cli.run(commands);
+})();
